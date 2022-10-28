2ef0f833d286ac4f1acb190fdf212e17dca42062
Poojitha <83769338+pooj124@users.noreply.github.com>
2022-10-27 21:28:04 -0500

    Update ValidAnagram.py

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -29,3 +29,14 @@ def isAnagram(self, s: str, t: str) -> bool:
             return True
         else:
             return False
+
+ #solution2
+#          if len(s) != len(t):
+#             return False
+
+#         countS, countT = {}, {}
+
+#         for i in range(len(s)):
+#             countS[s[i]] = 1 + countS.get(s[i], 0)
+#             countT[t[i]] = 1 + countT.get(t[i], 0)
+#         return countS == countT
